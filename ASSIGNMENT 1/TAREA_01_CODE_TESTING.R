7ebda7a97fa2701cb96a5467c129fa2688028a9e
Mauricio Vazquez <mvazq109@itam.mx>
2024-09-15 18:08:34 -0600

    Mods

--- ASSIGNMENT 1/TAREA_01_CODE_TESTING.R
+++ ASSIGNMENT 1/TAREA_01_CODE_TESTING.R
@@ -1,2 +1,14 @@
 # CODE TESTING SCRIPT
 
+## Librerias
+library(ggplot2)
+library(kableExtra)
+
+## Ejercicio 01
+
+### Lectura de datos
+data <- read.table(file = "datos_ej_1.txt", header = FALSE, sep = "|", strip.white = TRUE)
+names(data) <- c("Tamaños", "Num_horas")
+head(data)
+
+
