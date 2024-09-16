89f24e6d499c4710522ad9970f69dfe5fcd6541c
Mauricio Vazquez <mvazq109@itam.mx>
2024-09-15 19:41:30 -0600

    Actualizaciones

--- ASSIGNMENT 1/TAREA_01_CODE_TESTING.R
+++ ASSIGNMENT 1/TAREA_01_CODE_TESTING.R
@@ -11,5 +11,7 @@ data <- read.table(file = "datos_ej_1.txt", header = FALSE, sep = "|", strip.whi
 names(data) <- c("Tamaños", "Num_horas")
 head(data)
 
-
+data_02 <- read.table(file = "datos_ej_2.txt", header = FALSE, sep = "|", strip.white = TRUE)
+names(data_02) <- c("Servicio", "Equipos")
+head(data_02)
 
