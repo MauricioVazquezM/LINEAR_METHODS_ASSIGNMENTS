d7f32d16d68f732ed7d055097965f51981b7369d
Mauricio Vazquez <mvazq109@itam.mx>
2024-09-15 20:42:37 -0600

    Checkpoint pregunta I ej2

--- ASSIGNMENT 1/TAREA_01_CODE_TESTING.R
+++ ASSIGNMENT 1/TAREA_01_CODE_TESTING.R
@@ -24,4 +24,38 @@ ggplot(data_02, aes(x = Equipos, y = Servicio)) +
   labs(title = "Relación entre Servicio y Equipos", x = "Equipos", y = "Tiempo de Servicio (minutos)") +
   theme_minimal()
 
+residuales <- residuals(modelo)
+SSE <- sum(residuales^2)
+
+residuales_str <- toString(residuales)
+SSE_str <- toString(SSE)
+
+n <- length(residuales)
+
+sigmac_gorro <- sum(residuales^2) / (n - 2)
+sigma_gorro <- sqrt(sigmac_gorro)
+
+residuales <- residuals(modelo)
+SSE <- sum(residuales^2)
+
+x.datos <- data_02$Equipos
+x.barra <- mean(data_02$Equipos)
+abajo <- sum((x.datos - x.barra)^2)
+
+beta.1.gorro <- sum((data_02$Equipos - x.barra)*data_02$Servicio) / abajo
+alpha <- 1 - 0.90
+p <- 1-(alpha/2)
+
+vc <- qt(p = p, df = n - 2)
+y.gorro.dt <- predict(object = modelo)
+
+mse <- SSE/n
+
+sigma.2.gorro.b1 <- mse / abajo
+
+ls <- beta.1.gorro + vc*sqrt(mse/abajo)
+li <- beta.1.gorro - vc*sqrt(mse/abajo)
+
+summary_modelo <- summary(modelo)
+valor_p <- summary_modelo$coefficients["Equipos", "Pr(>|t|)"]
 
